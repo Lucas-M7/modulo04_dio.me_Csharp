3b4009d39ea41ace5837c6f4c932f76501d3121b
Lucas-M7 <lucas015x@gmail.com>
2024-01-14 15:56:15 -0300

    Exemplos práticos da classe Object

--- classe_abstrata/Program.cs
+++ classe_abstrata/Program.cs
@@ -3,25 +3,72 @@
 
 
 
+object objeto = "It's a object";
+
+//Métodos da Classe Object
+Console.WriteLine("ToString(): " + objeto.ToString());
+Console.WriteLine("GetHasCode(): " + objeto.GetHashCode());
+
+//Comparação de Referência
+object outroObjeto = "It's other object";
+Console.WriteLine("Comparação de referência: " + (objeto == outroObjeto));
+
+//Pode ser usado como tipo genérico
+var listaDeObjetos = new System.Collections.Generic.List<object>();
+listaDeObjetos.Add(23);
+listaDeObjetos.Add("Hello");
+
+//Unboxing
+int numero = (int)listaDeObjetos[0];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Classe Object
 
-List<object> listaDeObjeto = new List<object>();
-listaDeObjeto.Add("Hello, World! My Name is Michael Jordan");
-listaDeObjeto.Add(23);
+// List<object> listaDeObjeto = new List<object>();
+// listaDeObjeto.Add("Hello, World! My Name is Michael Jordan");
+// listaDeObjeto.Add(23);
 
 
-//Boxing and Unboxing
+// //Boxing and Unboxing
 
-int numero = 23;
-object objeto = numero; //Boxing
-int novoNumero = (int)objeto; //Unboxing
+// int numero = 23;
+// object objeto = numero; //Boxing
+// int novoNumero = (int)objeto; //Unboxing
 
-object qualquerCoisa = "It's a string";
-qualquerCoisa = 23; //Now is a number
+// object qualquerCoisa = "It's a string";
+// qualquerCoisa = 23; //Now is a number
 
-string texto1 = "Hello";
-string texto2 = "Hello";
-bool saoIguais = texto1 == texto2; //true (equal content, referências diferentes)
+// string texto1 = "Hello";
+// string texto2 = "Hello";
+// bool saoIguais = texto1 == texto2; //true (equal content, referências diferentes)
+
 
 
 
--- classe_abstrata/obj/Debug/net8.0/classe_abstrata.AssemblyInfo.cs
+++ classe_abstrata/obj/Debug/net8.0/classe_abstrata.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("classe_abstrata")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d43e06622ba11001603fd7521aec71d912c5e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("classe_abstrata")]
 [assembly: System.Reflection.AssemblyTitleAttribute("classe_abstrata")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
